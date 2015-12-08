658fd38cd6bc5c0974adae4795d56b1d834b2ba7
AnselGitAccount <ansel_blumers@brown.edu>
2015-12-08 17:11:22 -0500

    simulation result done, backup

--- Code/ErrFunct.m
+++ Code/ErrFunct.m
@@ -1,11 +1,14 @@
 Id_approx = W*W';
 Errfunct = sum(sum(Id_approx(logical(ones(N) - eye(N))))) + N*wmax^2-sum(sum(Id_approx(logical(eye(N)))));
 
+% concatenate
 if exist('Errfunct.mat', 'file')
     load('Errfunct.mat')
     E = cat(1, E, [rin, eta, epsilon, Errfunct]);
+    E = unique(E,'rows');
     save('Errfunct.mat', 'E');
 else
     E = [rin, eta, epsilon, Errfunct];
     save('Errfunct.mat', 'E');
 end
+
