b25071c2d1ef5c7dfc998d62ea361cde39b74651
Ankan Ganguly <gang0122@umn.edu>
2015-12-08 15:24:34 -0500

    Small change

--- Code/openfiles.m
+++ Code/openfiles.m
@@ -7,21 +7,18 @@ function openfiles(numFiles, Fileset)
         h(1) = figure;
         imagesc(logical(burstst'));
         title(['Bursts: rin=',num2str(rin), 'eta=', num2str(eta), 'epsilon=', num2str(epsilon)]);
-        savefig(['Bursts: rin_',num2str(rin), 'eta_', num2str(eta), 'epsilon_', num2str(epsilon), '.fig']);
         colorbar       
         
         %Plot weights
         h(2) = figure;
         imagesc(W);
         title(['Weights: rin=',num2str(rin), 'eta=', num2str(eta), 'epsilon=', num2str(epsilon)]);
-        savefig(['Bursts: rin_',num2str(rin), 'eta_', num2str(eta), 'epsilon_', num2str(epsilon), '.fig']);
         colorbar
         
         %Plot W*W'
         h(3) = figure;
         imagesc(W*W');
         title(['W*W: rin=',num2str(rin), 'eta=', num2str(eta), 'epsilon=', num2str(epsilon)]);
-        savefig(['Weightsnorm: rin_',num2str(rin), 'eta_', num2str(eta), 'epsilon_', num2str(epsilon), '.fig']);
         colorbar
         
         %Save the figures
