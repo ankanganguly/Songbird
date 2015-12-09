ea8d5b91ce7e7bc275b7312ca51c0b297a7ac918
Ankan Ganguly <gang0122@umn.edu>
2015-12-09 17:59:30 -0500

    Added papers played with BCM

--- Code/rough_BCM_implementation.m
+++ Code/rough_BCM_implementation.m
@@ -1,14 +1,15 @@
 %Parameters
 N = 10;                             %Number of inputs
 inputs = rand(N,1);                 %Inputs, for now assumed constant
+inputs(1) = 1;
 W = rand(1,N);                      %Initial weights
 theta = 0;                          %Initial threshold
 output = W*inputs;                  %Initial output
-steps = 1000000;                       %Number of steps
+steps = 100000;                      %Number of steps
 dt = 0.001;                         %Time interval
 tau_theta = 0.1;                    %Theta time constant
-jump = 1000;                          %Period of display
-epsilon = 0.1;                      %Weight decay
+ jump = 10;                        %Period of display
+epsilon = 0;                      %Weight decay
 
 %Recording variables
 Ws = zeros(steps+1,N);
@@ -23,8 +24,9 @@ inputset(:,1) = inputs;             %Record Inputs
 
 for t = 1:steps
     %Update threshold
-    theta = theta + (output^2 - theta)*dt/tau_theta;
-    %theta = exp(-(t+1 - (1:t))*dt/tau_theta)*(outputs(1:t).^2)*dt/tau_theta;
+    %theta = output^2;
+    %theta = theta + (output^2 - theta)*dt/tau_theta;
+    theta = exp(-(t+1 - (1:t))*dt/tau_theta)*(outputs(1:t).^2)*dt/tau_theta;
     thetas(t + 1) = theta;
     
     %Update weights
@@ -36,8 +38,8 @@ for t = 1:steps
     outputs(t + 1) = output;
     
     %Update inputs
-    inputs(1) = inputs(1) + (output - theta)*dt;
-    inputs(2:end) = inputs(2:end) + (theta - output)*dt;
+    inputs(1) = max(0,inputs(1) + (output - theta)*dt);
+    inputs(2:end) = max(0, inputs(2:end) + (theta - output)*dt);
     inputset(:,t+1) = inputs;
     
     if mod(t,10000) == 0
@@ -52,12 +54,12 @@ for t = 1:jump: steps + 1
     x = linspace(0, 5);
     y = x.*(x - thetas(t));
     plot(x,y);
-    y = linspace(0,3);
+    y = linspace(0,5);
     x = outputs(t)*ones(1,100);
     plot(x,y,'r');
     x = thetas(t)*ones(1,100);
     plot(x,y,'b');
-    scatter(Ws(t, :), linspace(1,3,N));
+    scatter(Ws(t, :), linspace(1,5,N));
     title(strcat('t = ', num2str(t)))
     pause(0.00000001);
 end
