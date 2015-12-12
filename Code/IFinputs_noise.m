4785a7b9200fb9d2b505bef00f16a13951fd3b5f
AnselGitAccount <ansel_blumers@brown.edu>
2015-12-12 18:43:22 -0500

    newer

--- Code/IFinputs_noise.m
+++ Code/IFinputs_noise.m
@@ -26,7 +26,7 @@ tau_STDP = 0.02;    %Time constant of learning in s
 tau_ada = 0.015;    %Inhibitary adaptation time constant
 
 %Initial variable values
-steps = 20000; 
+steps = 50000; 
 s = zeros(N,1);             %Activation 
 s_ada = zeros(N,1);         %Adaptation activation
 K = exp(0:-dt/tau_STDP:-(steps-1)*dt/tau_STDP)';
@@ -49,7 +49,7 @@ W = circshift(W,1);
 
 
 % add noise to Weight
-noise=wmax/10;
-W(7,1)=noise;
-W(8,2)=noise;
+% noise=wmax/10;
+% W(7,1)=noise;
+% W(8,2)=noise;
 
