a0275cf7ecfe231232cb00ba1eb613fe5f7ea21c
AnselGitAccount <ansel_blumers@brown.edu>
2015-12-13 21:33:12 -0500

    dfaasfasqgdgfdshrshtrehft

--- Code/plot_Error.m
+++ Code/plot_Error.m
@@ -2,7 +2,7 @@
 
 % load data
 load('Errfunct.mat')
-rin=Error(:,1)*1000;        % convert from ms to second.
+rin=Error(:,1);        % convert from ms to second.
 eta=Error(:,2);
 epl=Error(:,3);
 E=Error(:,4);
@@ -11,7 +11,7 @@ E=Error(:,4);
 % Error Values - All Runs
 figure()
 scatter(rin,E)
-xlim([2000 14000]*1000)
+xlim([2000 14000])
 % plot again (overlay), and add text on specific points
 hold on
 indx = find(E<0.83);
@@ -19,7 +19,7 @@ scatter(rin(indx), E(indx))
 label = strcat(' (',num2str(eta(indx)),', ',num2str(epl(indx)),')');
 text(rin(indx),E(indx),label,'HorizontalAlignment','left')
 legend('(\eta, \epsilon)','Location','southeast')
-xlabel('r_{in} (1/s)')
+xlabel('r_{in} Hz')
 ylabel('Error')
 title('All runs')
 hold off
@@ -32,7 +32,7 @@ prod=Error(:,2).*Error(:,3);
 c_prod = 0.1450;                        % the constant product
 indx = find(prod==c_prod);
 scatter(rin(indx),E(indx))
-xlim([2000 14000]*1000)
+xlim([2000 14000])
 % plot again (overlay), and add text on specific points
 hold on
 indx = find(prod==c_prod & E<0.9);
@@ -40,7 +40,7 @@ scatter(rin(indx),E(indx))
 label = strcat(' (',num2str(eta(indx)),', ',num2str(epl(indx)),')');
 text(rin(indx),E(indx),label,'HorizontalAlignment','left')
 legend('(\eta, \epsilon)','Location','southeast')
-xlabel('r_{in} (1/s)')
+xlabel('r_{in} Hz')
 ylabel('Error')
 title('Selected Runs; Constant \epsilon*\eta')
 hold off
